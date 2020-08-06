204f4f231dac9c7fdce2dd31085d0224f6526435
Suleyman1990 <suleymanov.s1990@gmail.com>
2020-08-05 20:34:15 -0500

    add MissJava.java

--- src/Utility/MissJava.java
+++ src/Utility/MissJava.java
@@ -1,9 +1,14 @@
 package Utility;
 
 public class MissJava {
+    public static void main(String[] args) {
 
 
-System.out.println("Asiya adam balasi kimi ders kecc")
-                   
+
+
+
+        System.out.println("Asiya adam balasi kimi ders kecc");
+    }
+
 
 }
